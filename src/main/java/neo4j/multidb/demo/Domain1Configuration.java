a960be1cae8d4b3f528ea7486c1b82f70b089ef0
Håkan Löfqvist <hakan.lofqvist@neo4j.com>
2020-08-07 11:47:40 +0200

    wip - still not working

--- src/main/java/neo4j/multidb/demo/Domain1Configuration.java
+++ src/main/java/neo4j/multidb/demo/Domain1Configuration.java
@@ -1,21 +1,30 @@
 package neo4j.multidb.demo;
 
 
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.core.DatabaseSelection;
-import org.springframework.data.neo4j.core.DatabaseSelectionProvider;
+import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableNeo4jRepositories(basePackages = "neo4j.multidb.demo.repository1")
+@EnableNeo4jRepositories(basePackages = "neo4j.multidb.demo.repository1", transactionManagerRef = "tm1")
 @EnableTransactionManagement
 public class Domain1Configuration {
 
+    private final Driver driver;
+
+    public Domain1Configuration(Driver driver) {
+        this.driver = driver;
+    }
+
     @Bean
-    DatabaseSelectionProvider databaseSelectionProvider1() {
-        return () -> DatabaseSelection.byName("db1");
+    Neo4jTransactionManager tm1() {
+        return new Neo4jTransactionManager(driver, () -> DatabaseSelection.byName("db1"));
     }
 
 }
--- src/main/java/neo4j/multidb/demo/Domain2Configuration.java
+++ src/main/java/neo4j/multidb/demo/Domain2Configuration.java
@@ -1,21 +1,29 @@
 package neo4j.multidb.demo;
 
 
+import org.neo4j.driver.Driver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.core.DatabaseSelection;
-import org.springframework.data.neo4j.core.DatabaseSelectionProvider;
+import org.springframework.data.neo4j.core.transaction.Neo4jTransactionManager;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableNeo4jRepositories(basePackages = "neo4j.multidb.demo.repository2")
+@EnableNeo4jRepositories(basePackages = "neo4j.multidb.demo.repository2", transactionManagerRef = "tm2")
 @EnableTransactionManagement
 public class Domain2Configuration {
 
-    @Bean
-    DatabaseSelectionProvider databaseSelectionProvider2() {
-        return () -> DatabaseSelection.byName("db2");
+    private final Driver driver;
+
+    public Domain2Configuration(Driver driver) {
+        this.driver = driver;
     }
+
+    @Bean
+    Neo4jTransactionManager tm2() {
+        return new Neo4jTransactionManager(driver,  () -> DatabaseSelection.byName("db2"));
+    }
+
 
 }
